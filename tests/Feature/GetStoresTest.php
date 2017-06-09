8ed6633db7268b3846cefb9c5d5dc6057a9da820
ArielEspinoza07 <arielespinoza46@gmail.com>
2017-06-09 12:15:38 -0400

    add unit testing

--- tests/Feature/GetStoresTest.php
+++ tests/Feature/GetStoresTest.php
@@ -14,13 +14,68 @@ class GetStoresTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testUnAuthorized()
+    {
+        //Arrange
+        $statusCode =   401;
+
+        //Act
+        $response = $this->json('GET','/api/v1/services/stores');
+
+        //Assert
+        $response->assertStatus($statusCode);
+    }
+
+    public function testGetSuccessStores()
     {
         //Arrange
         $statusCode =   200;
 
         //Act
-        $response = $this->json('GET','/api/v1/services/stores');
+        $headers    =   array(
+            'Authorization'    => 'Basic ' . base64_encode('my_user,my_password'),
+            'PHP_AUTH_USER'         => 'my_user',
+            'PHP_AUTH_PW'           => 'my_password'
+        );
+        $response   = $this->json('GET','/api/v1/services/stores',[],$headers);
+
+        //Assert
+        $response->assertStatus($statusCode);
+    }
+
+    public function testGetBadRequestStores()
+    {
+        //Arrange
+        $statusCode =   400;
+
+        //Act
+        $headers    =   array(
+            'Authorization'    => 'Basic ' . base64_encode('my_user,my_password'),
+            'PHP_AUTH_USER'         => 'my_user',
+            'PHP_AUTH_PW'           => 'my_password'
+        );
+        $data       =   array(
+            'city'  =>  'El Coyol'
+        );
+        $response   = $this->json('GET','/api/v1/services/stores',$data,$headers);
+
+        //Assert
+        $response->assertStatus($statusCode);
+    }
+
+    public function testGetNotFoundStoresArticles()
+    {
+        //Arrange
+        $statusCode =   404;
+
+        //Act
+        $headers    =   array(
+            'Authorization'    => 'Basic ' . base64_encode('my_user,my_password'),
+            'PHP_AUTH_USER'         => 'my_user',
+            'PHP_AUTH_PW'           => 'my_password'
+        );
+        $data       =   array();
+        $response   = $this->json('GET','/api/v1/services/stores/2/articles',$data,$headers);
 
         //Assert
         $response->assertStatus($statusCode);
